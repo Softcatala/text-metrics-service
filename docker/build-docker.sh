8fdcf6df1e257725fd2f58e14029676b294ef516
Jordi Mas <jmas@softcatala.org>
2021-01-23 19:24:09 +0100

    Initial docker

---
+++ docker/build-docker.sh
@@ -0,0 +1,1 @@
+docker build -t style-checker ../ -f dockerfile
---
+++ docker/entrypoint.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cd srv/web/
+mkdir -p /var/log/style-checker/
+gunicorn style-service:app -b 0.0.0.0:8000 --error-logfile /var/log/style-checker/gnuicorn.log --workers=8
