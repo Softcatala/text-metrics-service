ac8296cf020fa9c9517558a5a3896456ae66ed37
Jordi Mas <jmas@softcatala.org>
2024-04-09 07:15:55 +0200

    Fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 cd srv/web/
-gunicorn text-metrics:app -b 0.0.0.0:8000 --workers=1 --threads=1 --graceful-timeout 0 --timeout 0 --log-level=debug
+gunicorn text-metrics-service:app -b 0.0.0.0:8000 --workers=1 --threads=1 --graceful-timeout 0 --timeout 0 --log-level=debug
